a0f3e2be62c8390332cde4e809a030c50e385ae7
Vladyslav <ipz201_svo@student.ztu.edu.ua>
2021-01-29 12:46:46 +0200

    Add project files.

---
+++ ToDoListMvc/Data/GoalContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ToDoListMvc.Models;
+
+namespace ToDoListMvc.Data
+{
+    public class GoalContext : DbContext
+    {
+        public GoalContext(DbContextOptions<GoalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Goal> Goal { get; set; }
+    }
+}
